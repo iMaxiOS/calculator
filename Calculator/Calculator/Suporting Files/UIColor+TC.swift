7b3002492eecc14358aed4dba95e2e5b945218f3
iMaxiOS <ukr-maksim1@yandex.ru>
2018-07-03 18:04:15 +0300

    commit project

---
+++ Calculator/Calculator/Suporting Files/UIColor+TC.swift
@@ -0,0 +1,54 @@
+//
+//  UIColor+TC.swift
+//  Calculator
+//
+//  Created by iMaxiOS on 7/3/18.
+//  Copyright © 2018 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    
+    // MARK: Theme Colors
+    
+    static var tcDarkBlue: UIColor {
+        return UIColor(named: "tcDarkBlue")!
+    }
+    
+    static var tcOffWhite: UIColor {
+        return UIColor(named: "tcOffWhite")!
+    }
+    
+    static var tcHotPink: UIColor {
+        return UIColor(named: "tcHotPink")!
+    }
+    
+    static var tcCharcoal: UIColor {
+        return UIColor(named: "tcCharcoal")!
+    }
+    
+    static var tcAlmostBlack: UIColor {
+        return UIColor(named: "tcAlmostBlack")!
+    }
+    
+    static var tcMediumBlack: UIColor {
+        return UIColor(named: "tcMediumBlack")!
+    }
+    
+    static var tcBlueBlack: UIColor {
+        return UIColor(named: "tcBlueBlack")!
+    }
+    
+    static var tcSeafoamGreen: UIColor {
+        return UIColor(named: "tcSeafoamGreen")!
+    }
+    
+    static var tcBlack: UIColor {
+        return UIColor(named: "tcBlack")!
+    }
+    
+    static var tcWhite: UIColor {
+        return UIColor(named: "tcWhite")!
+    }
+}
