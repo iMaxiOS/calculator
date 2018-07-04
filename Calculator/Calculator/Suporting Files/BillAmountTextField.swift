dafd926adfd63d0c2464914e0e307bd6e7de6b8b
iMaxiOS <ukr-maksim1@yandex.ru>
2018-07-04 10:16:47 +0300

    add_new_BillAmountTextField_files

---
+++ Calculator/Calculator/Suporting Files/BillAmountTextField.swift
@@ -0,0 +1,72 @@
+//
+//  BillAmountTextField.swift
+//  Calculator
+//
+//  Created by iMaxiOS on 7/4/18.
+//  Copyright © 2018 Maxim Granchenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BillAmountTextField: UITextField {
+    
+    var buttonTapAction: (() -> Void)?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let toolbar: UIToolbar = UIToolbar()
+        
+        let leadingFlex = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let trainingFlex = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let calculateButton = UIBarButtonItem(title: "Calculate Tip", style: .done, target: self, action: #selector(doneButtonTapped(_:)))
+        
+        toolbar.items = [leadingFlex, calculateButton, trainingFlex]
+        
+        toolbar.sizeToFit()
+        
+        self.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButtonTapped(_ sender: UIBarButtonItem) {
+        buttonTapAction?()
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
